3e5ff726c1f34d898b9b042eb6c06ed058de7625
DigiCoders Technologies <digicodersdevelopment@gmail.com>
2025-11-25 11:54:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv';
+dotenv.config();
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/adminpanel");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error.message);
